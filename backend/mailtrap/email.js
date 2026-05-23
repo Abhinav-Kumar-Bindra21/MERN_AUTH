2c9e1341abe5f04c8c82157abe69ff77100bc7fc
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-23 16:41:46 +0530

    Done welcome email feature

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -19,3 +19,27 @@ export const sendVerificationEmail = async (email, verificationCode) => {
     throw new Error(`Error sending verification email:${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "8001c126-5d70-4e0c-8bc0-1f86e9d5c9ce",
+      template_variables: {
+        company_info_name: "JUST BELEIVE",
+        name: name,
+        company_info_address: "KHALI NAGAR",
+        company_info_city: "DELHI",
+        company_info_zip_code: "13722",
+        company_info_country: "INDIA",
+      },
+    });
+
+    console.log("Welcome Email sent successfully", response);
+  } catch (error) {
+    console.log(`Error sending welcome email`, error);
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
