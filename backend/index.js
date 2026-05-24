c45336a985a81b8a67a056838a010449568ea667
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-24 22:01:30 +0530

    create middleware verifyToken

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import { connectDb } from "./configs/db.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(400).json({ success: false, message: "Unauthorized, no token provided" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) return res.status(400).json({ success: true, message: "Unauthorized, invalid token " });
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    console.log("Error is verifyToken", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
