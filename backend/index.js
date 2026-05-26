f7f580e481aa98f590797895fe487d7c1cd37286
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-26 12:35:18 +0530

    Connect frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import { connectDb } from "./configs/db.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use(cookieParser());
 app.use(express.json());
